819b58b603d250806de6466fe60886f20f98af39
yongsu <kys752@gmail.com>
2016-08-30 16:17:50 +0900

    Let's learn english

---
+++ src/yaho/english.java
@@ -0,0 +1,9 @@
+package yaho;
+
+public class english {
+	public static void main(String[] args) {
+		System.out.println("영어를 배우자");
+		System.out.println("Step1");
+		
+	}
+}
---
+++ src/yaho/yaliyali.java
@@ -0,0 +1,7 @@
+package yaho;
+
+public class yaliyali {
+	public static void main(String[] args) {
+		System.out.println("abc");
+	}
+}
